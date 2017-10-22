466e87efdc694982ca0e44b9113db95dcb0b989b
MichelleCersosimo <michellemarie.c@hotmail.com>
2017-10-21 23:40:53 -0600

    mas estilo

--- search.py
+++ search.py
@@ -1,8 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import Helpers.myparser as parser
 import Helpers.db as db
-import Helpers.structure
+import Helpers.structure as structure
+import Helpers.nav as nav
 import os, sys
 
 print("Content-Type: text/html\r\n\r\n")
@@ -11,7 +12,10 @@ print("Content-Type: text/html\r\n\r\n")
 #print(os.getenv("REQUEST_METHOD"))
 
 if os.getenv("REQUEST_METHOD") == 'GET':
-	item = parser.parseData(os.getenv("QUERY_STRING"))
 	db.connectDB()
+	autenticate = db.checkSession(parser.parseCookie(os.getenv("HTTP_COOKIE")))
+	structure.printStartSection()
+	nav.printNav(autenticate)
+	item = parser.parseData(os.getenv("QUERY_STRING"))
 	data = db.search(item['search'])
-	Helpers.structure.printItemContents(data)
+	structure.printItemContents(data)
